cd23ea577a57d484ac41c03548192ad6d7cef0a2
Michiel Sikma <michiel@privax.com>
2015-03-20 09:15:03 +0000

    Also copy PyCharm

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Sed
-cat ~/Library/Preferences/WebStorm9/colors/DadaExportScheme.icls > ./DadaColors.icls
+cat ~/Library/Preferences/WebStorm9/colors/DadaExportScheme.icls > ./DadaColorsWs.icls
+cat ~/Library/Preferences/PyCharm40/colors/DadaPython.icls > ./DadaColorsPy.icls
